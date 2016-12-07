03c2c60a035b3387b2bdf51117dda5eb4a7f2898
Hamze Dokoohaki <hamzed@iastate.edu>
2016-12-06 21:02:32 -0600

    Having two treatments for N application

--- BioCro-Nfert.R
+++ BioCro-Nfert.R
@@ -16,15 +16,28 @@ phenolcont<-phenoParms(tp1 = 400, tp2 = 600,tp3 = 1000,tp4 = 1200,
                        kGrain6 = 0.55)
 
 
+                         # Fertilization, # initial mineral N
+centuryContNofert<-centuryParms(Nfert = c(0,90),iMinN = 0)
+centuryContfert<-centuryParms(Nfert = c(10000,90),iMinN = 100000)
 
-centuryCont<-centuryParms(Nfert = c(99,00))
+nitrocont<-nitroParms(Vmax.b1=-1,alpha.b1=-1)
+nitrocont2<-nitroParms()
 
-modelc<- BioGro(weather05,timestep = 1,day1 = 90,dayn = 300,
+modelNoN<- BioGro(weather05,timestep = 1,day1 = 90,dayn = 300,
                 soilControl=s0,
                 phenoControl=phenolcont,
-                centuryControl = centuryCont)
+                centuryControl = centuryContNofert, ## ading the fert
+                nitroControl = nitrocont)   # control the N effect on growth
 
 
-#plot(modelc)
+modelN<- BioGro(weather05,timestep = 1,day1 = 90,dayn = 300,
+                soilControl=s0,
+                phenoControl=phenolcont,
+                centuryControl = centuryContfert, ## ading the fert
+                nitroControl = nitrocont)   # control the N effect on growth
 
-barplot(modelc$SNpools)
+
+## Stem
+xyplot(modelNoN$Stem+modelN$Stem~modelNoN$DayofYear)
+##vmax
+xyplot(modelNoN$VmaxVec+modelN$VmaxVec~modelNoN$DayofYear)
